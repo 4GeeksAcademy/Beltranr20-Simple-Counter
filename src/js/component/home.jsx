c3ffc5879cfde2fe5c8fec6ca45f5d08b578a6e2
Randy Beltran <131041473+Beltranr20@users.noreply.github.com>
2023-08-08 19:03:54 +0000

    debugged port issue

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
-import Digit from "./Digit";
+import Digit from "./digit";
 
-//created component
+//create your first component
 const Home = () => {
     let [counter, setCounter] = useState(0)
     useEffect(()=> {
         setInterval(() => {
-            setCounter(counter++) 
+            setCounter(counter++)
         }, 1000);
     },[]) 
+    // can set up however many places values you want
     return (
         <div className="text-center">
+            <Digit alpha={Math.floor(counter/1000000)}/>
+            <Digit alpha={Math.floor(counter/100000)}/>
             <Digit alpha={Math.floor(counter/10000)}/>
             <Digit alpha={Math.floor(counter/1000)}/>
             <Digit alpha={Math.floor(counter/100)}/>
